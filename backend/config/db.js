134d0fbfe3d0d11df830ff8cac05b906861e6942
dagi <dagimashenafidagi@gmail.com>
2026-05-01 09:04:43 -0700

    Prepare backend for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,14 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
+
 });
 
 // Test the database connection upon initialization
